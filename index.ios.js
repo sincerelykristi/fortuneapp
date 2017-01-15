74f17accb7eca101b158f13e2dbdc7ff073130aa
Kristi Murphy <kristilmurphy@gmail.com>
2017-01-14 22:43:39 -0500

    install axios and move source folder

--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class FortuneApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
 
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
+AppRegistry.registerComponent('FortuneApp', () => App);
