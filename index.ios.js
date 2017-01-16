f05bd9fced1c0ebd90c927a7286079b435378a76
Kristi Murphy <kristilmurphy@gmail.com>
2017-01-15 19:36:16 -0500

    seperate App and add andriod

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
-
-const App = () => {
-  return (
-    <Text>Some Text</Text>
-  );
-};
 
 AppRegistry.registerComponent('FortuneApp', () => App);
